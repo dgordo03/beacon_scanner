6a6c1dde7712f47aa4da514794d18b3bb3be9c5f
Daniel Gordon <daniel.gordon.0427@gmail.com>
2017-11-12 12:12:24 -0500

    Adding auto push from c file

--- src/git_push.sh
+++ src/git_push.sh
@@ -1,4 +1,3 @@
-cp ~/dump.txt files/dump.txt
 git add files/dump.txt
 git commit -m "Pushing data to server"
 git push origin master
--- src/main.c
+++ src/main.c
@@ -1,15 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+typedef struct {
+    int rssi;
+    int beacon;
+    int row;
+    int column;
+} Beacon;
+
+/*The distance from the BLE is calculated using the RSSI value
+*Input: Beacon structure
+*Output: The distance as a float
+*/
+float distance(Beacon beacon)
+{
+
+}
+
+/*Data is written to a file at a specified location
+*Input: the location of the file, the information to be written to that file
+*Output: null
+*/
+void writeToFile(char *fileLoc, char *info)
+{
+    FILE *fp;
+    fp = fopen(fileLoc, "w");
+    fprintf(fp, "%s", info);
+    fclose(fp);
+}
+
+void pushToGit()
+{
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    strcat(cwd, "/src/git_push.sh");
+    system("bash %s", cdw);
+}
+
 int main()
 {
     char *fileLoc;
     char *fileName;
     int index;
+    int beacon_config;
+    Beacon beacon;
     fileLoc = getenv("HOME");
     fileName = "/dump.txt";
     strcat(fileLoc, fileName);
     index = 0;
+    beacon_config = 0;
     //while (1)
     //{
         FILE *fp;
@@ -26,13 +66,36 @@ int main()
             }
             else if (curChar == '\n')
             {
-                printf("%s\n", curLine);
+                char *a;
+                a = strstr(curLine, "ADV_NONCONN_IND");
+                if (a != NULL)
+                {
+                    beacon_config = 1;
+                }
+                if (beacon_config == 1)
+                {
+                    a = strstr(curLine, "RSSI");
+                    if (a != NULL)
+                    {
+                        char *token;
+                        token = strtok(curLine, ":");
+                        token = strtok(NULL, ":");
+                        beacon.rssi = atoi(token);
+                        beacon_config = 0;
+                    }
+                }
                 index = 0;
                 memset(curLine, '\0', 100);
             }
         } while (curChar != EOF);
-        printf("%s\n", curLine);
         fclose(fp);
     //}
     return 0;
 }
+
+/* This is how to print to the correct file
+char cwd[1024];
+getcwd(cwd, 1024);
+strcat(cwd, "/files/beacon.txt");
+writeToFile(cwd, token);
+*/

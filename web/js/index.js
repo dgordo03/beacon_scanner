6841c7d0c7b0474e6c080cfa25490b06e34a119a
Daniel Gordon <daniel.gordon.0427@gmail.com>
2017-11-12 20:57:31 -0500

    convert to JSON

--- web/js/index.js
+++ web/js/index.js
@@ -1,16 +1,17 @@
 function loadData()
 {
-
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           console.log(xhttp.responseText);
+           jsonParse(xhttp.responseText);
         }
     };
-    // Initialize a request
     xhttp.open('get', 'https://dgordo03.github.io/beacon_scanner/files/default.txt');
-    // Send it
     xhttp.send();
 }
-loadData();
+
+function jsonParse(toParse)
+{
+   var obj = JSON.parse(toParse);
+   console.log(obj); 
+}

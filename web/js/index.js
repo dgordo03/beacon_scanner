09ad231bc19571ed167de490c9de9b7317e75933
Daniel Gordon <daniel.gordon.0427@gmail.com>
2017-11-12 20:29:28 -0500

    print out the info

--- web/js/index.js
+++ web/js/index.js
@@ -1,11 +1,16 @@
 function loadData()
 {
 
-    var request = new XMLHttpRequest();
-
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           console.log(xhttp.responseText);
+        }
+    };
     // Initialize a request
-    request.open('get', 'https://dgordo03.github.io/beacon_scanner/files/default.txt');
+    xhttp.open('get', 'https://dgordo03.github.io/beacon_scanner/files/default.txt');
     // Send it
-    request.send();
+    xhttp.send();
 }
 loadData();

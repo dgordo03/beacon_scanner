cb2e470afb73e4792dd5c7458f0eb20296e05d7f
Daniel Gordon <daniel.gordon.0427@gmail.com>
2017-11-14 15:19:00 -0500

    get the correct file, continuously update the data of the parked cars JSON file

--- web/js/index.js
+++ web/js/index.js
@@ -8,13 +8,17 @@ function loadData()
            jsonParse(xhttp.responseText);
         }
     };
-    xhttp.open('get', 'https://dgordo03.github.io/beacon_scanner/files/default.txt');
+    xhttp.open('get', 'https://dgordo03.github.io/beacon_scanner/files/beacon.txt');
     xhttp.send();
 }
 
 function jsonParse(toParse)
 {
    var obj = JSON.parse(toParse);
-   console.log(obj); 
+   console.log(obj);
 }
 
+function updateData()
+{
+  setTimeout(loadData(), 5000);
+}
